227a2b4f7c5838840c1fad41f39a1852d2db5e0f
Char1que <char1k59@mail.ru>
2022-09-23 19:47:18 +0300

    Error
    Error

--- src/App.js
+++ src/App.js
@@ -1,62 +1,72 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import item from "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
-  const welcome = 'Добро пожаловать в чат';
-  const [messageList, setMessageList] = useState ([]);
-  const [text, setText] = useState ('');
-  const [author, setAuthor] = useState ('');
-  const hangleSubmit  = (event) => {
+  const welcome = "Добро пожаловать в чат";
+  const [messageList, setMessageList] = useState([]);
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const hangleSubmit = (event) => {
     event.preventDefault();
-    setMessageList(prevState => [...prevState, {
-      id: giveLastID,
-      text: text,
-      author: author
-    }])
-    setText ('')
-    setAuthor ('')
-  }
+    setMessageList((prevState) => [
+      ...prevState,
+      {
+        id: giveLastID,
+        text: text,
+        author: author,
+      },
+    ]);
+    setText("");
+    setAuthor("");
+  };
   const giveLastID = (array) => {
-    return array.length ? array[array.length - 1].id + 1 : 0
-  }
+    return array.length ? array[array.length - 1].id + 1 : 0;
+  };
 
-  useEffect (() => {
-    setTimeout (() => {
-      botAnswer()
-    }, 2000)
-    }, [messageList])
+  useEffect(() => {
+    setTimeout(() => {
+      botAnswer();
+    }, 2000);
+  }, [messageList]);
 
   const botAnswer = () => {
     const lastAuthor = messageList[messageList.length - 1];
     if (lastAuthor && lastAuthor.author) {
-      setMessageList(prevState => [
-        ...prevState, {
+      setMessageList((prevState) => [
+        ...prevState,
+        {
           id: giveLastID(prevState),
-          text: `Сообщение от ${lastAuthor.author} отправлено`
-        }
-      ])
+          text: `Сообщение от ${lastAuthor.author} отправлено`,
+        },
+      ]);
     }
-  }
+  };
   return (
     <div>
-      <h1>
-        {welcome}
-      </h1>
+      <h1 className={item.welcome}>{welcome}</h1>
       <div>
         <form onSubmit={hangleSubmit}>
-          <input value={author} onChange={(event) => setAuthor (event.target.value)}/>
-          <input value={text} onChange={(event) => setText(event.target.value)}/>
-          <button type='submit'>Отправить сообщение</button>
+          <input
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+          />
+          <input
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <Button className={item.sending} variant="outlined">Отправить сообщение</Button>
         </form>
-        {messageList.map((message => {
-        return (
-          <div key={message.id}>
-            {message.author}
-            {message.text}
-          </div>
-        )
-      }),)} </div>
+        {messageList.map((message) => {
+          return (
+            <div key={message.id}>
+              {message.author}
+              {message.text}
+            </div>
+          );
+        })}{" "}
+      </div>
     </div>
   );
 }
